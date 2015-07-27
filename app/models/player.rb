89616639567c68b9f73b82d5d6b89001e0811321
Eric Lehner <ericj@buffalo.edu>
2015-07-26 21:36:01 -0400

    Created base team view
    Added functions to handle score calculations for total and round

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,4 +5,15 @@ class Player < User
   has_many :flags, foreign_key: :user_id # contains all flags over each round
   has_many :flag_submissions, foreign_key: :user_id # contains both their submissions and the other teams
   
+
+  def score
+    points = 0
+    points
+  end
+
+  def score_for_round
+    points = 0
+    points
+  end
+
 end
