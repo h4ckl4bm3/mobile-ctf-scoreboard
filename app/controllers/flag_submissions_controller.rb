304fc3b8c0bbc2cf32a9aaa61f2cdfa3b3db4587
Eric Lehner <ericj@buffalo.edu>
2015-08-07 16:40:55 -0400

    Added flag submission view and ensured submission works

--- app/controllers/flag_submissions_controller.rb
+++ app/controllers/flag_submissions_controller.rb
@@ -1,9 +1,8 @@
 class FlagSubmissionsController < ApplicationController
   before_filter :enforce_access
-
   def index
     # Shows flag submitted listing (which team, when, and success)
-    @flag_submissions = current_user.flag_submissions
+    @flag_submissions = current_user.flag_submissions.order(submitted_at: :desc)
   end
 
   def show
@@ -14,9 +13,15 @@ class FlagSubmissionsController < ApplicationController
     #Ensure they are not capturing their own flag or the same team's flag multiple times
 
     # Used to submit a new flag
-    defending_team = User.find(params[:team])
-    new_fs = FlagSubmission.create(user: current_user, flag: params[:flag], owner: defending_team)
-    @defending_team = defending_team
-    @success = true
+    defending_team = User.find_by_id(params[:team])
+    defending_team = nil unless defending_team
+    if defending_team == current_user
+      @defending_team = "NICE TRY"
+      @success = true
+    else
+      new_fs = FlagSubmission.create(user: current_user, flag: params[:flag], owner: defending_team)
+      @defending_team = new_fs.owner.display_name if new_fs.owner
+      @success = new_fs.success
+    end
   end
 end
