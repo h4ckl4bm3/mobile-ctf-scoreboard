5d8246ebf62563800c3431403a16f7e69363368b
Eric Lehner <ericj@buffalo.edu>
2015-08-17 20:34:32 -0400

    Add base for team users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @players = [@player]
 
     # @flags_submitted = to_timeline FlagSubmission.where("user_id=?",params[:id]).group_by {|sf| sf.updated_at.change(:sec=>0)}
-    # @score = @player.score
+    @score = @player.score
     @title = @player.display_name
 
     render :show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :teams, controller: 'users',  only: [:index, :show]
-
+  get 'myteam', :to => 'users#myteam'
   # devise
   devise_for :users
   devise_scope :user do
