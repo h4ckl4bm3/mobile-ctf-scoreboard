6eb3b66fcc178caec12e50ce0b10616b8f5633f9
Eric Lehner <ericj@buffalo.edu>
2015-08-21 17:07:32 -0400

    Base for countdown timer for periods
    Will display in the footer of each page.  Should possibly also countdown to the next period if possible.

--- app/assets/javascripts/users.coffee
+++ app/assets/javascripts/users.coffee
@@ -1,3 +1,24 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+startTimer(endTime) ->
+	countdown = (endTime) ->
+		diff = endTime - new Date()
+		diff = 0 if diff < 0
+		hours = Math.floor(diff/1000/60/60)
+		diff -= hours * 1000*60*60
+		minutes = Math.floor(diff/1000/60)
+		diff -= minutes * 1000*60
+		seconds = Math.floor(diff/1000)
+		return {"hours": hours, "minutes":minutes, "seconds":seconds}
+
+	String.prototype.paddingLeft = (paddingValue) ->
+		String(paddingValue + this).slice(-paddingValue.length)
+
+	updateClock = () ->
+		difference = countdown(new Date(endTime*1000))
+		$("#hours").text(difference.hours.toString().paddingLeft("00")+ "h")
+		$("#minutes").text(difference.minutes.toString().paddingLeft("00") + "m")
+		$("#seconds").text(difference.seconds.toString().paddingLeft("00")+ "s")
+	updateClock()
+	setInterval(updateClock, 1000)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,33 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception 
-  
+  protect_from_forgery with: :exception
+
   include SessionsHelper
   include ActionView::Helpers::TextHelper
-  
-  before_filter :load_round, :load_messages_count
+
+  before_filter :load_period, :load_messages_count
 
   def enable_auto_reload
     @auto_reload = false
   end
-  
-  def load_round
+
+  def load_period
     # @round = Round.instance
     # raise ActiveRecord::RecordNotFound unless @round
     unless current_user.nil?
       now = Time.now # This will eventually be used for Rounds
+      @period = AttackPeriod.find_by('start <= :now and :now <= finish', {now: now})
+      @period = DefendPeriod.find_by('start <= :now and :now <= finish', {now: now}) unless @period
     end
   end
-  
+
   def load_messages_count
     unless current_user.nil?
       @messages_count = current_user.messages.count
     end
   end
-  
+
   def enforce_access
     deny_access unless current_user
   end
